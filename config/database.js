340cf3f7e20cf5af355c04d326d77a073ae0e773
Joao Avelino <joaoavelino@MacBook-Air-de-Joao.local>
2021-12-28 10:27:31 -0300

    Erro na inicialização do servidor do mongo

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
-var mongoose = require('mongoose');
+
+//1)require do mongoose
+var mongoose = require("mongoose");
+//2) conectar com a promisse global
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/javascriptNote', {
-}
-).then(() => console.log('connection succesful'))
+mongoose.connect("mongodb://localhost/javascriptnote", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conectado ao MongoDb"))
   .catch((err) => console.error(err));
